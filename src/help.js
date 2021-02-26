80e3f3fd7fab9b09d5afb048529bf17ae612218f
Meliodas-rai <79414899+Meliodas-rai@users.noreply.github.com>
2021-02-25 22:14:15 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,94 @@
+const help = (*) => {
+
+	return `> Eae meu parça, meu nome e Ban o bot do Meliodas, use os comandos abaixo
+	para eu fazer o que pedir, *serei seu escravo*
+	
+	Base: Causs
+	Canal do criador: https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g
+	Modificado por: Meliodas
+	
+	
+	
+	
+	
+	*Comados de figurinhas* <
+comando : *${prefix}sticker* or *${prefix}stiker*
+descrição: converter imagem/gif/video em adesivo
+uso: responder imagem/gif/vídeo ou enviar imagem/gif /vídeo com legenda\n
+comando : *${prefix}sticker nobg* or *${prefix}stiker nobg*
+descrição : converter imagem em adesivo removendo o fundo
+uso : responder imagem ou enviar imagem com legenda\n 
+comando : *${prefix}toimg*
+descrição : converter adesivo em imagem
+uso : transforma figurinha em imagem\n
+comando : *${prefix}tsticker* or *${prefix}tstiker*
+descrição : converter texto em adesivo
+uso : *${prefix}texto do adesivo aqui*\n
+> *Comandos de memes😂* <
+comando : *${prefix}meme*
+descrição : imagens aleatórias de meme [portuguese]
+uso : apenas envie o comando\n
+comando : *${prefix}memeindo*
+descrição : imagens aleatórias de meme [portuguese]
+uso : apenas envie o comando\n
+> *Mais comandos meu chapa* <
+comando : *${prefix}gtts*
+descrição : convert text to speech/audio
+uso : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja Onii-chan*\n
+comando : *${prefix}loli*
+descrição : imagens aleatórias de loli
+uso : apenas envie o comando\n
+comando : *${prefix}nsfwloli*
+descrição : imagens aleatórias de nsfw loli
+uso : basta enviar o comando\n
+comando : *${prefix}url2img*
+descrição : tirar screenshots da web
+uso : *${prefix}url2img [tipe] [url]*\n
+comando : *${prefix}simi*
+descrição : sua mensagem será respondida por simi
+uso : *${prefix}simi sua menssagem*\n
+comando : *${prefix}ocr*
+descrição : pegue o texto na foto
+uso : responder imagem ou enviar imagem com legenda\n
+comando : *${prefix}wait*
+descrição : procure anime com imagem [ que anime tem isso / aquilo ]
+uso : responder imagem ou enviar imagem com legenda\n
+comando : *${prefix}setprefix*
+descrição : substituir prefixo
+uso : *${prefix}setprefix [texto|opcional]*\nexample : *${prefix}setprefix !*
+nota : Este comando só pode ser usado pelo proprietário do bot\n
+comando : *${prefix}add*
+descrição : adicionar membro ao grupo
+uso : *${prefix}add 62813xxxxx*\n
+nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin!\n
+comando : *${prefix}kick*
+descrição : expulsa membros do grupo
+uso : *${prefix}kick @tagdomenbro*\n
+nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin!\n
+comando : *${prefix}promote*
+descrição : tornar o membro do grupo como administrador do grupo
+uso : *${prefix}promote @tagmember*\n
+nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin!\n
+comando : *${prefix}demote*
+descrição : tornar o administrador do grupo como membro do grupo
+uso : *${prefix}demote @tagdomembro*\n
+nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin!\n
+comando : *${prefix}linkgroup*
+descrição : pegue o link do grupo
+uso : apenas envie o comando
+nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin!\n
+comando : *${prefix}leave*
+descrição : Faz o bot sair do grupo
+uso : apenas envie o comando
+nota : Só pode ser usado por administradores de grupo e proprietário do bot\n
+comando : *${prefix}tagall*
+descrição : marca todos os membros do grupo, incluindo administradores também
+uso : apenas envie o comando
+nota : Este comando pode ser usado se voçê for um administrador de grupo\n
+comando : *${prefix}simih*
+descrição : ative o modo simi no grupo
+uso : *${prefix}simih 1* para ativar o modo simi e *${prefix}simih 0* para desativar o modo simi
+nota : Este comando pode ser usado se voçê for um administrador de grupo\n`
+}
+
+exports.help = help
